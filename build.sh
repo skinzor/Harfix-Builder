3fe4d8dcff97b8bc4c20c84310e9d41ce69cb55b
wisniew <rafciowis1999pl@wp.pl>
2016-07-26 23:01:13 +0200

    build.sh update

--- build.sh
+++ build.sh
@@ -10,9 +10,13 @@
 # REMEMBER TO EDIT LOCATIONS!!!
 
 
-##################
-# Normal version #
-##################
+# Let's start!
+
+# Clean before build
+rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/Harfix3-MALI_fix.zip
+rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/Harfix3.zip
+rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/boot/bootimg/*
+rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/system/lib/modules/*
 
 # Prepering
 export ARCH=arm
@@ -20,6 +24,11 @@ export CROSS_COMPILE=/home/gemi/TC/gcc-linaro-5.3-2016.02/bin/arm-linux-gnueabih
 
 # Cleaning
 make clean
+
+
+##################
+# Normal version #
+##################
 
 # Load config
 make custom_i9300_defconfig
@@ -31,10 +40,6 @@ make -j4
 find -name '*.ko' -exec cp -av {} /home/gemi/git/Harfix3/harfix3/normal/modules/ \;
 cp /home/gemi/git/Harfix3/arch/arm/boot/zImage /home/gemi/git/Harfix3/harfix3/normal/boot/
 
-# Remove old files
-rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/boot/bootimg/*
-rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/system/lib/modules/*
-
 # copy from harfix3 to ZIP_FILES
 cp /home/gemi/git/Harfix3/harfix3/normal/modules/* /home/gemi/git/Harfix3/harfix3/ZIP_FILES/system/lib/modules/
 cp /home/gemi/git/Harfix3/harfix3/normal/boot/zImage /home/gemi/git/Harfix3/harfix3/ZIP_FILES/boot/bootimg/
@@ -43,6 +48,9 @@ cp /home/gemi/git/Harfix3/harfix3/normal/boot/zImage /home/gemi/git/Harfix3/harf
 cd /home/gemi/git/Harfix3/harfix3/ZIP_FILES/
 zip -qry -9 "Harfix3.zip" . -x "*.zip"
 cd /home/gemi/git/Harfix3
+
+# move zip to main Harfix3 folder
+mv /home/gemi/git/Harfix3/harfix3/ZIP_FILES/Harfix3.zip /home/gemi/git/Harfix3/harfix3/
 
 
 
@@ -72,6 +80,9 @@ cp /home/gemi/git/Harfix3/harfix3/MALI_fix/boot/zImage /home/gemi/git/Harfix3/ha
 cd /home/gemi/git/Harfix3/harfix3/ZIP_FILES/
 zip -qry -9 "Harfix3-MALI_fix.zip" . -x "*.zip"
 cd /home/gemi/git/Harfix3
+
+# move zip to main Harfix3 folder
+mv /home/gemi/git/Harfix3/harfix3/ZIP_FILES/Harfix3-MALI_fix.zip /home/gemi/git/Harfix3/harfix3/
 
 # Clean
 rm /home/gemi/git/Harfix3/harfix3/ZIP_FILES/boot/bootimg/*
